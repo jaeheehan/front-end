1a2e75cb3aa1f9f74f9667e6b19464fd72e71b71
jaeheehan <jaehee@i-screamedu.co.kr>
2020-02-17 17:44:58 +0900

    VUEX, MODAL

---
+++ src/components/PageHeader.vue
@@ -0,0 +1,23 @@
+<template>
+
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'PageHeader',
+  computed: {
+    ...mapGetters([
+      'user', 'hasBoards', 'personalBoards', 'teamBoards'
+    ])
+  },
+  created () {
+    this.$store.dispatch('getMyData')
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,24 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import * as getters from './getters'
+import * as actions from './actions'
+import mutations from './mutations'
+import createLogger from 'vuex/dist/logger'
 
 Vue.use(Vuex)
 
+const state = {
+  user: {
+    name: null
+  },
+  teams: [],
+  boards: []
+}
+
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+  state,
+  getters,
+  mutations,
+  actions,
+  plugins: process.env.NODE_ENV !== 'production' ? [createLogger()] : []
 })
