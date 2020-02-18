b97b7ba041931d1c59029b7a09ae9453a66d00c3
jaeheehan <jaehee@i-screamedu.co.kr>
2020-02-18 10:53:45 +0900

    다국어, 폰트아썸 추가

--- src/main.js
+++ src/main.js
@@ -4,6 +4,10 @@ import router from './router'
 import store from './store'
 import axios from 'axios'
 import Vuelidate from 'vuelidate'
+import { library as faLibrary } from '@fortawesome/fontawesome-svg-core'
+import { faHome, faSearch, faPlus } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { i18n } from './i18n'
 
 axios.defaults.baseURL = '/api'
 axios.defaults.headers.common.Accept = 'application/json'
@@ -17,8 +21,13 @@ axios.interceptors.response.use(
 Vue.use(Vuelidate)
 Vue.config.productionTip = false
 
+// Set up FontAwesome
+faLibrary.add(faHome, faSearch, faPlus)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
 new Vue({
   router,
   store,
+  i18n,
   render: h => h(App)
 }).$mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import HomePage from '@/views/HomePage'
 import LoginPage from '../views/LoginPage'
 import RegisterPage from '../views/RegisterPage'
 Vue.use(VueRouter)
@@ -8,6 +9,11 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomePage
+    },
     {
       path: '/login',
       name: 'LoginPage',
